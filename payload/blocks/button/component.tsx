7de5936e5c17e4826445af1f4a4111866c7c3067
GGK <ggkk7376@gmail.com>
2025-12-17 23:50:17 +0100

    Fix: Revert Button component to stable state (disable links)

--- payload/blocks/button/component.tsx
+++ payload/blocks/button/component.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { cache, type ReactNode } from "react";
 
 import External from "@/assets/icons/external.svg";
@@ -9,16 +8,12 @@ import { Text } from "@/components/text";
 import { getPayloadClient } from "@/lib/payload";
 import type { Button as ButtonProps, Dialog } from "@/payload-types";
 
-function ButtonContent({
-  label,
-  isLink,
-}: { label: string; isLink?: boolean }) {
-  const Comp = isLink ? "div" : "button";
+function ButtonContent({ label }: { label: string }) {
   return (
     <EnterAnimationBlur className="flex cursor-pointer items-center justify-center gap-1 rounded-lg border border-[#212121] bg-[radial-gradient(66%_93%_at_48%_0%,#262626_0%,var(--color-dark-600)_76%)] px-7 py-4 font-sans">
       <Flipper asChild>
         <Text variant="p2" asChild>
-          <Comp type={isLink ? undefined : "button"}>
+          <button type="button">
             <span>{label}</span>
             <FlipperContent
               className="h-6 w-6"
@@ -26,22 +21,14 @@ function ButtonContent({
             >
               <External />
             </FlipperContent>
-          </Comp>
+          </button>
         </Text>
       </Flipper>
     </EnterAnimationBlur>
   );
 }
 
-export function Button({ dialog, label, type, url }: ButtonProps) {
-  if (type === "link" && url) {
-    return (
-      <Link href={url}>
-        <ButtonContent label={label} isLink />
-      </Link>
-    );
-  }
-
+export function Button({ dialog, label }: ButtonProps) {
   return (
     <DialogTrigger dialog={dialog}>
       <ButtonContent label={label} />
